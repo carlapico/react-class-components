7257b8ac140659d4cd21e297d06d0c9c7ff163ce
Carla Pico <carlaspico@gmail.com>
2022-08-15 17:02:47 -0400

    added changes to the components and the read.md

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import ClassComponent from './ClassComponent';
-import FunctionalComponents from './FunctionalComponents';
+import ClassComponent from './components/ClassComponent';
+import FunctionalComponents from './components/FunctionalComponents';
 import './App.css';
 
 let count =  1 // bad practice to declasre it outside
